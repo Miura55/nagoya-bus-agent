cd2985fdbfeb752f1e66ed23c56adce80a59a980
Miura55 <acordion.piano@gmail.com>
2025-11-21 17:41:00 +0900

    Add initial project structure for Nagoya Bus Agent
    - Created .gitignore to exclude Python-generated files and virtual environments.
    - Added .python-version to specify Python version 3.13.
    - Included MIT License file.
    - Implemented main.py with an agent for providing Nagoya city bus information.
    - Created pyproject.toml for project metadata and dependencies.
    - Added README.md with installation instructions.
    - Generated uv.lock for dependency management.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Nagoya BUS Agent
+
+名古屋の市バスの運行情報を調べるエージェントです
+
+## Require
+- uv
+
+## Install
+
+```
+uv sync
+```
---
+++ main.py
@@ -0,0 +1,42 @@
+from mcp import stdio_client, StdioServerParameters
+from strands import Agent, tool
+from strands.tools.mcp import MCPClient
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+SYSTEM_PROMPT = """
+あなたは名古屋の市営バスについての情報を提供するエージェントです。
+以下の要件を満たしてください。
+- ユーザーが入力した内容に対して、名古屋の市営バスについての情報を提供してください。
+- 出てきた返答を元に該当する系統の時刻表をテーブル形式で表示してください。
+- 時刻表には該当する系統の時刻表が存在しない場合は「該当する系統の時刻表が存在しません。」と表示してください。
+- 現在時刻を取得するために、get_current_time()を使用してください。
+"""
+
+mcp_client = MCPClient(lambda:stdio_client(
+        StdioServerParameters(
+            command="uvx",
+            args=["nagoya-bus-mcp"]
+        )
+    ),
+)
+
+
+@tool
+def get_current_time():
+    return datetime.now(ZoneInfo("Asia/Tokyo")).strftime("%Y-%m-%d %H:%M:%S")
+
+
+
+with mcp_client:
+    # Get the tools from the MCP server
+    tools = mcp_client.list_tools_sync() + [get_current_time]
+
+    # Create the agent
+    agent = Agent(
+        tools=tools,
+        system_prompt=SYSTEM_PROMPT
+    )
+    # Run the agent
+    prompt = input("Enter a prompt: ")
+    result = agent(prompt)
