c2562358aaa25ce52554dd4277d682f3ab5af1ae
Miura55 <acordion.piano@gmail.com>
2026-03-22 12:39:27 +0900

    名古屋バスエージェントの初期実装を追加

---
+++ agent/main.py
@@ -0,0 +1,74 @@
+"""
+名古屋の市バスの運行情報を提供するエージェントです
+"""
+import os
+import logging
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from mcp import stdio_client, StdioServerParameters
+from strands import Agent, tool
+from strands.tools.mcp import MCPClient
+from bedrock_agentcore.runtime import BedrockAgentCoreApp
+
+
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+logging.basicConfig(
+    level=getattr(logging, LOG_LEVEL, logging.INFO),
+    format="%(asctime)s %(levelname)s %(name)s - %(message)s",
+)
+logger = logging.getLogger("strands")
+
+app = BedrockAgentCoreApp()
+
+SYSTEM_PROMPT = """
+あなたは名古屋の市営バスについての情報を提供するエージェントです。
+以下の要件を満たしてください。
+- ユーザーが入力した内容に対して、名古屋の市営バスについての情報を提供してください。
+- 出てきた返答を元に該当する系統の時刻表をテーブル形式で表示してください。
+- 時刻表には該当する系統の時刻表が存在しない場合は「該当する系統の時刻表が存在しません。」と表示してください。
+- 現在時刻を取得するために、get_current_time()を使用してください。
+"""
+
+mcp_client = MCPClient(lambda:stdio_client(
+        StdioServerParameters(
+            command="uvx",
+            args=["nagoya-bus-mcp@latest"]
+        )
+    ),
+)
+
+
+@tool
+def get_current_time():
+    return datetime.now(ZoneInfo("Asia/Tokyo")).strftime("%Y-%m-%d %H:%M:%S")
+
+
+@app.entrypoint
+async def entrypoint(payload):
+    """アプリケーションのエンドポイント
+
+    Args:
+        payload: クライアントからの入力
+    """
+    logger.info("Received payload: %s", payload)
+    user_prompt = payload.get("prompt", "")
+
+    with mcp_client:
+        # Get the tools from the MCP server
+        tools = mcp_client.list_tools_sync() + [get_current_time]
+
+        # Create the agent
+        agent = Agent(
+            tools=tools,
+            system_prompt=SYSTEM_PROMPT
+        )
+
+        # Run Agent
+        stream_messages= agent.stream_async(user_prompt)
+        async for message in stream_messages:
+            if "event" in message:
+                yield message
+
+
+if __name__ == "__main__":
+    app.run()
